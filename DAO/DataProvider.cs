f1e642e8bcbad9ee3d8ab14a764b870ffc02c104
DangHongThy <1554050096thy@ou.edu.vn>
2019-03-21 11:08:33 +0700

    tao project

---
+++ DAO/DataProvider.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAO
+{
+    public class DataProvider
+    {
+        public SqlConnection cn;
+
+        public DataProvider()
+        {
+            string cnStr = "Data Source=.;Initial Catalog=CoffeeShop;Integrated Security=True";
+            cn = new SqlConnection(cnStr);
+        }
+        public void Connect()
+        {
+            try
+            {
+                if (cn.State == ConnectionState.Closed)
+                {
+                    cn.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void DisConnect()
+        {
+            if (cn.State == ConnectionState.Open)
+            {
+                cn.Close();
+            }
+        }
+
+        public int MyExecuteScalar(string sql)
+        {
+            Connect();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandText = sql;
+            cmd.CommandType = System.Data.CommandType.Text;
+
+            int numberOfRows;
+            numberOfRows = (int)cmd.ExecuteScalar();
+
+            DisConnect();
+
+            return numberOfRows;
+        }
+
+        public SqlDataReader MyExecuteReader(string sql)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandText = sql;
+            cmd.CommandType = System.Data.CommandType.Text;
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            return dr;
+        }
+        public int MyExecuteNonQuery(string sql)
+        {
+            Connect();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = sql;
+
+            int numberOfRows = cmd.ExecuteNonQuery();
+
+            DisConnect();
+            return numberOfRows;
+        }
+    }
+}
---
+++ DAO/Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace DAO
+{
+    public class Users:DataProvider
+    {
+        public bool Login(string user, string pass)
+        {
+            string sql = "SELECT COUNT(UserName) FROM Users WHERE UserName = '" + user + "' AND Password = '" + pass + "' ";
+
+            int numberOfRows = MyExecuteScalar(sql);
+            if (numberOfRows > 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}

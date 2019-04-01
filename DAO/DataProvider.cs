78fbae0aafb624095bb0bb4be99222f540fed5a2
Trahoang96 <1554050102tra@ou.edu.vn>
2019-04-01 11:02:05 +0700

    tao form Product va xu ly form

--- DAO/DataProvider.cs
+++ DAO/DataProvider.cs
@@ -13,7 +13,7 @@ namespace DAO
 
         public DataProvider()
         {
-            string cnStr = "Data Source=.;Initial Catalog=CoffeeShop;Integrated Security=True";
+            string cnStr = @"Data Source=.\SQLEXPRESS;Initial Catalog=CoffeeShop1;Integrated Security=True";
             cn = new SqlConnection(cnStr);
         }
         public void Connect()
